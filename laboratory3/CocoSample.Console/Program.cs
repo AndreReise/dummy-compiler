fa3ab255751418d473204f6ae0a5403196d6e9bf
Andrii Babanin <andrii.babanin@gmail.com>
2024-04-02 19:56:26 +0300

    Implement laboratory 3 (Coco).

---
+++ laboratory3/CocoSample.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CocoSample.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var fs = File.Open("./Text/test.fakec", FileMode.Open, FileAccess.Read);
+
+            var scanner = new Scanner(fs);
+            var parser = new Parser(scanner);
+
+            parser.Parse();
+
+            System.Console.WriteLine("{0} errors detected", parser.errors.count);
+        }
+    }
+}
